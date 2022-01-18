a4f8ee1b143073174d83443299b564c812bd4960
clesyo.silva <clesyo.silva@maidahealth.com>
2022-01-18 09:24:17 -0300

    Commit inicial

---
+++ src/main/java/br/com/delivery/motoboy/DelivaeryMotoboyApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.delivery.motoboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelivaeryMotoboyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelivaeryMotoboyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/delivery/motoboy/DelivaeryMotoboyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.delivery.motoboy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelivaeryMotoboyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
