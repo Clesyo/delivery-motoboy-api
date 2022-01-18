95a78983b552191ba17858ead5309c5a1970626f
clesyo.silva <clesyo.silva@maidahealth.com>
2022-01-18 13:26:40 -0300

    Criando modelo base de usuario

--- src/main/java/br/com/motoboy/delivery/DelivaeryMotoboyApiApplication.java
+++ src/main/java/br/com/motoboy/delivery/DelivaeryMotoboyApiApplication.java
@@ -1,4 +1,4 @@
-package br.com.delivery.motoboy;
+package br.com.motoboy.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
